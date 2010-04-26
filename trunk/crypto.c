6ea35f0b47021d9359412786945a705f42503511
vikaskumar11 <vikaskumar11@0226992e-485b-8998-48f6-96fcd75c9bd9>
2010-04-26 00:32:18 +0000

    Implemented RSA modules

--- trunk/crypto.c
+++ trunk/crypto.c
@@ -1,37 +1,49 @@
 #include <openssl/evp.h>
 #include <openssl/aes.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <string.h>
 
-#define SERVER_CERT "server.pem"
+#define SERVER_CERT "rootcert.pem"
 #define RSA_PUB_SIZE RSA_size(crypto_ctx.pub_key)
 #define RSA_PRIV_SIZE RSA_size(crypto_ctx.priv_key)
 
+#define STATUS_SUCCESS 0
+#define STATUS_FAILURE 1
+
+typedef int status_t;
 
 struct server_enc_ctx {
   EVP_CIPHER_CTX aes_ctx;
   RSA *pub_key;
   RSA *priv_key;
+  EVP_PKEY *pkey; 
 } crypto_ctx;
 
 typedef struct server_enc_ctx server_enc_ctx_t;
 
-int aes_init(unsigned char *key_data, int key_data_len, EVP_CIPHER_CTX *e_ctx, 
-             EVP_CIPHER_CTX *d_ctx)
+int aes_init(unsigned char *key_data, int key_data_len, char *key_out, char *iv_out)
 {
   int i, nrounds = 5;
-  unsigned char key[32], iv[32];
+  //unsigned char key[32], iv[32];
   
-  i = EVP_BytesToKey(EVP_aes_256_cbc(), EVP_sha1(), NULL, key_data, key_data_len, nrounds, key, iv);
+  i = EVP_BytesToKey(EVP_aes_256_cbc(), EVP_sha1(), NULL, key_data, key_data_len, nrounds, key_out, iv_out);
   if (i != 32) {
     printf("Key size is %d bits - should be 256 bits\n", i);
     return -1;
   }
 
-  EVP_CIPHER_CTX_init(e_ctx);
-  EVP_EncryptInit_ex(e_ctx, EVP_aes_256_cbc(), NULL, key, iv);
-  EVP_CIPHER_CTX_init(d_ctx);
-  EVP_DecryptInit_ex(d_ctx, EVP_aes_256_cbc(), NULL, key, iv);
-
   return 0;
+}
+
+void aes_enc_init(char *key, char *iv) {
+  EVP_CIPHER_CTX_init(&crypto_ctx.aes_ctx);
+  EVP_EncryptInit_ex(&crypto_ctx.aes_ctx, EVP_aes_256_cbc(), NULL, key, iv);
+}
+
+void aes_dec_init(char *key, char *iv) {
+  EVP_CIPHER_CTX_init(&crypto_ctx.aes_ctx);
+  EVP_DecryptInit_ex(&crypto_ctx.aes_ctx, EVP_aes_256_cbc(), NULL, key, iv);
 }
 
 unsigned char *aes_encrypt(EVP_CIPHER_CTX *e, unsigned char *plaintext, int *len)
@@ -67,24 +79,52 @@ void inline aes_cleanup(EVP_CIPHER_CTX *ctx) {
 }
 
 status_t server_crypto_init() {
-  FILE *cert_fp = NULL;
+  FILE *cert_fp = NULL, *fp = NULL;
+  X509 *x509 = NULL;
 
+  memset(&crypto_ctx, 0, sizeof(crypto_ctx));
+
+  OpenSSL_add_all_algorithms();
   cert_fp = fopen(SERVER_CERT, "rb");
 
   if(cert_fp == NULL)
     return STATUS_FAILURE;
 
-  if(!PEM_read_RSA_PublicKey(cert_fp, &crypto_ctx.pub_key, NULL, NULL)) {
-    fclose(cert_fp);
+  x509= PEM_read_X509(cert_fp, NULL, NULL, NULL);
+  if(x509 == NULL) {
+    printf("Cant read x509\n");
     return STATUS_FAILURE;
   }
 
-  if(!PEM_read_RSA_PrivateKey(cert_fp, &crypto_ctx.priv_key, NULL, NULL)) {
+  crypto_ctx.pkey = X509_get_pubkey(x509);
+  if(crypto_ctx.pkey == NULL) {
+     printf("Cant get x509 pubkey\n");     
+     return STATUS_FAILURE;
+  }
+
+  crypto_ctx.pub_key = EVP_PKEY_get1_RSA(crypto_ctx.pkey);
+
+  if(crypto_ctx.pub_key == NULL) {
+    printf("Error getting rsa key\n");
+    return STATUS_FAILURE;
+  }
+
+  fp = fopen("serverkey.pem", "rb");
+  
+  if(fp == NULL) {
+   printf("cannot open file\n");
+   return STATUS_FAILURE;
+  }
+
+  if((crypto_ctx.priv_key = PEM_read_RSAPrivateKey(fp, NULL, NULL, NULL)) == NULL) { 
+    ERR_print_errors_fp(stderr);
     fclose(cert_fp);
     return STATUS_FAILURE;
   }
 
   fclose(cert_fp);
+  fclose(fp);
+
   return STATUS_SUCCESS;
 }
 
@@ -94,4 +134,12 @@ int pub_encrypt(char *src, int len, char *dst) {
 
 int priv_decrypt(char *src, int len, char *dst) {
   return (RSA_private_decrypt(len, (unsigned char *)src, dst, crypto_ctx.priv_key, RSA_PKCS1_PADDING));
+}
+
+int main() {
+  if(server_crypto_init() == STATUS_SUCCESS) {
+    printf("RSA Size: %d\n", RSA_PUB_SIZE);
+  }		    
+
+  return 0;
 }
